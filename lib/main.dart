706e3140756010804d6ebf13f7580484e7fb7fa5
Nikola Sowa <xenchantra@gmail.com>
2026-03-31 19:08:23 +0200

    lab 5 - krakflow navigation and add task screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,92 @@
 import 'package:flutter/material.dart';
+//import 'task_repository.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  List<Task> tasks = [
-    Task(
-      title: "Wyjsc z psem",
-      deadline: "o 12",
-      done: true,
-      priority: "wysoki",
-    ),
-    Task(
-      title: "Umyc podlogi",
-      deadline: "po salonie",
-      done: false,
-      priority: "niski",
-    ),
-    Task(
-      title: "Posprzatac salon",
-      deadline: "do 15",
-      done: false,
-      priority: "sredni",
-    ),
-    Task(
-      title: "Zrobic zakupy",
-      deadline: "do 10",
-      done: true,
-      priority: "wysoki",
-    ),
-  ];
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    int doneCounter = tasks.where((task) => task.done).length;
+    return MaterialApp(home: HomeScreen());
+  }
+}
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("KrakFlow")),
-        body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      "Liczba zadan: ${tasks.length}, zrobiono: ${doneCounter}",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      "Dzisiejsze zadania",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    int doneCounter = TaskRepository.tasks.where((task) => task.done).length;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("KrakFlow")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Liczba zadan: ${TaskRepository.tasks.length - doneCounter}, zrobiono: ${doneCounter}",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Dzisiejsze zadania",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index) {
-                    return TaskCard(
-                      title: tasks[index].title,
-                      subtitle: "termin: ${tasks[index].deadline} | priorytet: ${tasks[index].priority}",
-                      icon: tasks[index].done ? Icons.check_circle : Icons.radio_button_unchecked,
-                    );
-                  },
-                ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: TaskRepository.tasks.length,
+                itemBuilder: (context, index) {
+                  return TaskCard(
+                    title: TaskRepository.tasks[index].title,
+                    subtitle:
+                        "termin: ${TaskRepository.tasks[index].deadline} | priorytet: ${TaskRepository.tasks[index].priority}",
+                    icon: TaskRepository.tasks[index].done
+                        ? Icons.check_circle
+                        : Icons.radio_button_unchecked,
+                  );
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final Task? newTask = await Navigator.push(
+            context,
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  AddTaskScreen(),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                    return FadeTransition(opacity: animation, child: child);
+                  },
+            ),
+          );
+          if (newTask != null) {
+            setState(() {
+              TaskRepository.tasks.add(newTask);
+            });
+          }
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
@@ -119,6 +125,97 @@ class TaskCard extends StatelessWidget {
         leading: Icon(icon),
         title: Text(title),
         subtitle: Text(subtitle),
+      ),
+    );
+  }
+}
+
+class TaskRepository {
+  static List<Task> tasks = [
+    Task(
+      title: "Wyjsc z psem",
+      deadline: "o 12",
+      done: true,
+      priority: "wysoki",
+    ),
+    Task(
+      title: "Umyc podlogi",
+      deadline: "po salonie",
+      done: false,
+      priority: "niski",
+    ),
+    Task(
+      title: "Posprzatac salon",
+      deadline: "do 15",
+      done: false,
+      priority: "sredni",
+    ),
+    Task(
+      title: "Zrobic zakupy",
+      deadline: "do 10",
+      done: true,
+      priority: "wysoki",
+    ),
+  ];
+}
+
+//-------------------------------------------------------------------------
+
+class AddTaskScreen extends StatelessWidget {
+  AddTaskScreen({super.key});
+
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController deadlineController = TextEditingController();
+  final TextEditingController priorityController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Nowe zadanie")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 8),
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: "Tytuł zadania",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: deadlineController,
+              decoration: InputDecoration(
+                labelText: "Deadline",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: priorityController,
+              decoration: InputDecoration(
+                labelText: "Priorytet",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                final newTask = Task(
+                  title: titleController.text,
+                  deadline: deadlineController.text,
+                  done: false,
+                  priority: priorityController.text,
+                );
+                Navigator.pop(context, newTask);
+              },
+              child: Text("Zapisz"),
+            ),
+          ],
+        ),
       ),
     );
   }
